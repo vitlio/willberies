fcb399b15e8c1ca5f78d5ce8636240c8d1234c8f
vitlio <gorshkov.vitalik@yandex.ru>
2021-03-26 00:17:25 +0300

    3d day update + homework

--- js/main.js
+++ js/main.js
@@ -14,20 +14,114 @@ const modalCart  	   = document.getElementById('modal-cart');
 const modalClose 	   = document.querySelector('.modal-close');
 const modal     	   = document.querySelector('.modal');
 const overlay  		   = document.querySelector('.overlay');
+const cartTableGoods   = document.querySelector('.cart-table__goods');
+const cardTableTotal   = document.querySelector('.card-table__total');
+const cartCount		   = document.querySelector('.cart-count');
 
 setTimeout(function d(){sliderButtonNext.click(); setTimeout(d, 3000)}, 3000);
 
-const openModal  = function() {
+const getData  = async function() {
+	let result = await fetch('../db/db.json')
+		.then(response => {
+			if(!response.ok){
+				console.error('Ошибочка: ' + response.status)
+			}
+			return response;})
+		return await result.json();
+}
+
+const cart = {
+	cartGoods: [],
+	cartCounter(){
+		cartCount.textContent = this.cartGoods.reduce((sum, item) => sum + item.count, 0);
+	},
+	renderCart(){
+		cartTableGoods.textContent = "";
+		this.cartGoods.forEach(({id, name, price, count}) => {
+			const trGoods = document.createElement('tr');
+			trGoods.classList.add('cart-item');
+			trGoods.dataset.id = id;
+			trGoods.innerHTML = `
+				<td>${name}</td>
+				<td>${price}$</td>
+				<td><button class="cart-btn-minus" data-id=${id}>-</button></td>
+				<td>${count}</td>
+				<td><button class="cart-btn-plus" data-id=${id}>+</button></td>
+				<td>${price * count}$</td>
+				<td><button class="cart-btn-delete" data-id=${id}>x</button></td>
+			`;
+			cardTableTotal.textContent = this.cartGoods.reduce((sum, item) => {
+				return sum + item.price * item.count;
+			}, 0) + '$';
+			cartTableGoods.append(trGoods);
+		});
+		this.cartCounter();
+	},
+	minusGood(id){
+		let o = this.cartGoods.find(obj => obj.id == id);
+		o.count--;
+		o.count <=0 ? this.deleteGood(o.id) : this.renderCart();
+
+	},
+	plusGood(id){
+		let o = this.cartGoods.find(obj => obj.id == id);
+		o.count++;
+		this.renderCart();
+	},
+	deleteGood(id){
+		this.cartGoods = this.cartGoods.filter(item => item.id !== id)
+		this.renderCart();
+	},
+	addCartGood(id){
+		if(this.cartGoods.find(item => item.id === id)){
+			this.plusGood(id)
+			} else {
+			getData()
+				.then(arr => arr.find(item => item.id === id))
+				.then(({id, name, price}) => {
+					this.cartGoods.push({
+						id,
+						name,
+						price,
+						count: 1
+					});
+					this.cartCounter();
+				});
+			
+		}
+		
+	}
+};
+
+const openModal  = () => {
+	cart.renderCart();
 	modalCart.classList.add('show');
 };
-const closeModal = function() {
-	modalCart.classList.remove('show');
- };
 
+const closeModal = () => modalCart.classList.remove('show');
+
+document.body.addEventListener('click', e => {
+	if(e.target.closest('.add-to-cart'))
+		cart.addCartGood(e.target.closest('.add-to-cart').dataset.id)
+});
 buttonCart.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
 overlay.addEventListener('click', e => {
-	if(!e.target.closest('.modal')) closeModal();
+	if(!e.target.closest('.modal')) {
+	closeModal();
+	};
+	if(e.target.classList.contains('cart-btn-delete')){
+	cart.deleteGood(e.target.closest('.cart-item').dataset.id);
+	};
+	if(e.target.classList.contains('cart-btn-plus')){
+		if(e.target.tagname = 'button')
+		cart.plusGood(e.target.closest('.cart-item').dataset.id);
+	};
+	if(e.target.classList.contains('cart-btn-minus')){
+		if(e.target.tagname = 'button')
+		cart.minusGood(e.target.closest('.cart-item').dataset.id);
+	};
+
 });
 //-----------------------------------------------scroll smooth
 
@@ -52,25 +146,14 @@ overlay.addEventListener('click', e => {
 const more 			 = document.querySelector('.more');
 const navigationLink = document.querySelectorAll('.navigation-link');
 const longGoodsList  = document.querySelector('.long-goods-list');
-const logoLink = document.querySelector('.container');
+const logoLink 		 = document.querySelector('.container');
 
-const getData  = async function() {
-	let result = await fetch('../db/db.json')
-		.then(response => {
-			if(!response.ok){
-				console.error('Ошибочка: ' + response.status)
-			}
-			return response;})
-		return await result.json();
-}
-
-const renderCard  = function(obj){
+const renderCard   = function(obj){
 	const card     = document.createElement('div');
 	card.className = 'col-lg-3 col-sm-6';
 	card.innerHTML = `
 			<div class="goods-card">
-				${obj.label ? `<span class="label">New</span>` : ''}
-				
+				${obj.label ? `<span class="label">New</span>` : ''}		
 				<img src="db/${obj.img}" alt="${obj.name}" class="goods-image">
 				<h3 class="goods-title">${obj.name}</h3>
 				<p class="goods-description">${obj.description}</p>
@@ -144,5 +227,6 @@ buttons.forEach(btn => {
 	})
 
 })
+//-----------------------------------------------banners event
 
 
